860d8dad058445fbeb5c38907fc73ae1a7520384
Ruben Adrados <rubenbreton@gmail.com>
2014-12-16 11:40:03 +0100

    Update Test Sctipts

--- test/scripts/ActualizarKernel-2.sh
+++ test/scripts/ActualizarKernel-2.sh
@@ -7,4 +7,7 @@ apt-get --assume-yes install linux-image-generic-lts-raring linux-headers-generi
 #Copiamos el instalador de Docker en la carpeta init.d para que se ejecute con el resto de servicios del sistema
 sudo cp InstalarDocker-2.sh /etc/init.d/
 
+#Lo instalamos
+sudo update-rc.d InstalarDocker-2.sh defaults
+
 reboot
