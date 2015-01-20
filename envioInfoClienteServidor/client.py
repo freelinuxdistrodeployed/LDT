d7b2fde7d8d0824e7ba68886b64e87432a92efa6
Ubuntu <ansibleServer@AnsibleServer15.AnsibleServer15.f1.internal.cloudapp.net>
2015-01-20 10:31:21 +0000

    mod client

--- envioInfoClienteServidor/client.py
+++ envioInfoClienteServidor/client.py
@@ -1,12 +1,32 @@
 #Cliente
-import socket
 
+#1. Aniadimos librerias necesarias:
+import socket 
+import sys
+
+#2. Creamos un objeto de tipo socket
 s=socket.socket()
-host=socket.gethostname()
+
+#3. Especificamos la IP del servidor, de dos maneras posibles:
+#3.1. Dandola manualmente
+host='100.85.98.28'
+
+#3.2 .Conseguimos la ip del servidor por su nombre de dominio
+try:
+   host_ip=socket.gethostbyname('ansibleserver15.cloudapp.net')
+except socket.gaierror:
+   #Cuando no pueda resover el nombre del server:
+   print "No ha sido posible resolver el nombre del server"
+   sys.exit();
+print "la ip del host sever es",host_ip
+
+
 port=12345
 
 #Realizacion de la conexion:
-s.connect((host,port))
+s.connect((host_ip,port))
+
+print "the socket has successfully connected"
 
 #Imprimimos el mensaje que confirma la conexion:
 print "Cliente:",s.recv(1024)
@@ -16,3 +36,4 @@ s.send("hello");
 
 #Cerramos la conexion por parte del cliente:
 s.close
+
