188f305ebded25992c78b02b88d91b43007167b2
Mario Ortega Aguayo <mortega87@gmail.com>
2015-01-20 13:02:07 +0100

    Añadido script acceso_usuarios

---
+++ scripts/acceso_usuarios.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Script para conocer usuarios del sistema y ultima vez que accedieron al host
+#./acceso_usuarios.sh
+ansible all -a "lastlog" > salida.txt
+
+numHost=`wc -l /etc/ansible/hosts`
+numHost=${numHost:0:1}
+
+echo -e "Usuarios en hosts:\n"
+
+for((i=1; i<numHost;i++))
+do
+        nombreHOSTn=`cat salida.txt | grep -m${i} ".cloudapp" | tail -n1`
+        nombreHOSTn=${nombreHOSTn:0:5}
+        echo -e ${nombreHOSTn}"\n"
+
+        while read linea;
+        do
+                echo $linea
+	done < salida.txt
+        echo -e "\n"    
+done
+
+rm salida.txt
+
+
