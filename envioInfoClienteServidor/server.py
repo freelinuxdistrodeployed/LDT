8a3bebdb2d0070b47b95335fe80f61c4bb3fa810
azure provisioned user <usuario@Host1.Host12014.f1.internal.cloudapp.net>
2015-01-20 10:30:41 +0000

    Mod server.py

--- envioInfoClienteServidor/server.py
+++ envioInfoClienteServidor/server.py
@@ -4,23 +4,25 @@ import socket
 
 
 #Para escribir en un fichero debemos de abrirlo:
-ficheroHosts=open("file","w")
+#ficheroHosts=open("file","w")
 
-ficheroHosts.write("hola holita vecinito")
-ficheroHosts.write("\n")
+#ficheroHosts.write("hola holita vecinito")
+#ficheroHosts.write("\n")
 
 
 
 #Creamos un objeto de tipo socket
-s=socket.socket();
+s=socket.socket()
+print "Socket creado con exito"
 
 #Obtenemos el nombre de la maquina local:
-host=socket.gethostname();
+host=socket.gethostname()
+
 #Establecemos un puerto para la escucha:
 port=12345
 
 #Enlazamos los datos al socket creado:
-s.bind((host,port));
+s.bind(('',port))
 
 s.listen(5);
 while True:
@@ -31,8 +33,8 @@ while True:
 
     print 'Servidor: Recibida conexion desde:',addr
 
-    ficheroHosts.write(addr)
-    ficheroHosts.write("\n")
+#    ficheroHosts.write(addr)
+#    ficheroHosts.write("\n")
 
 
     #Establecida la conexion debemos recibir los datos desde el cliente y escribirlos en hosts:
